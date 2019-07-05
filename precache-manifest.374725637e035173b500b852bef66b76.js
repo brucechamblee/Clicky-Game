60a377be0976719095f1a2c79bbde37cf35dec48
bruce chamblee <brucechamblee@gmail.com>
2019-07-05 16:33:20 -0400

    Updates

---
+++ precache-manifest.374725637e035173b500b852bef66b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3658eabda1d11e0bab154cc1ef62699b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "88a65fb7bd11b103f5b6",
+    "url": "/Clicky-Game/static/css/main.7b6ccc70.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "88a65fb7bd11b103f5b6",
+    "url": "/Clicky-Game/static/js/main.9520d86f.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
